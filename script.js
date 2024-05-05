81c1843307c869d34650d480ea9c2b1f9465aab4
Viet Nguyen <viet2003.vn@gmail.com>
2024-05-05 16:54:21 +0300

    fixed animation

---
+++ script.js
@@ -0,0 +1,21 @@
+const urlToQuery = 'https://api.adviceslip.com/advice';
+let advice = ''
+const getAdvice = async () => {
+  try{
+    const response = await fetch (urlToQuery)
+    if(response.ok) {
+      const jsonResponse = await response.json()
+      console.log(jsonResponse)
+      document.getElementById('advice').innerHTML = jsonResponse.slip.advice;
+      document.getElementById('advice').style.animation = "fadeIn 0.5s";
+      const timerId = setTimeout(() => {
+        document.getElementById('advice').style.animation = "none";
+      }, 3000)
+      
+    }
+  }catch(error){
+    console.log(error)
+  }
+
+}
+
